81da42248f4178af0c3c31f300e209588cbae482
unknown <bkarahodzi1@etf.unsa.ba>
2025-12-21 22:20:12 +0100

    add migrations for creating admin and users

---
+++ server/src/main/resources/db/migration/V11__add_users_to_db.sql
@@ -0,0 +1,22 @@
+INSERT INTO users (email, password, role, created_at, updated_at)
+VALUES (
+           'user1@cinemaapp.com',
+           '$2a$10$AB67e1In8nZ9C3Lr44E8.uyw6n9zRBASGiI5XZUkLzuI/oP2W1DtC',
+           'USER',
+           now(),
+           now()
+       ),
+       (
+           'user2@cinemaapp.com',
+           '$2a$10$YsPOdcKMcoFYJiSbiys/3e7rsrAHbNtdc16pmkmz2bBsDNgycZNfG',
+           'USER',
+           now(),
+           now()
+       ),
+       (
+           'user3@cinemaapp.com',
+           '$2a$10$ovrizyyQo6tykWXXxJOA7erxWy11WiR/dWqmxZRFsfKRsSICTKDRK',
+           'USER',
+           now(),
+           now()
+       )
